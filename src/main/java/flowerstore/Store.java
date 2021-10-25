87f024260689c9155dbaf1297b702fc36fd9610e
Stefan <49642756+elem3ntary@users.noreply.github.com>
2021-10-25 19:01:41 +0300

    Updated JDK version for Github Actions

--- src/main/java/flowerstore/Store.java
+++ src/main/java/flowerstore/Store.java
@@ -1,7 +1,5 @@
 package flowerstore;
 
-import jdk.jshell.spi.ExecutionControl;
-
 public class Store {
     public void search(String query){
 //        System.out.println("Searching some flowers fo u!");
